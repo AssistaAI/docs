b3d0936c1429ed3683004289e209d3e5d4789065
antonzaharia <zaharia_anton@yahoo.com>
2024-01-10 08:25:07 +0000

    docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'AssistaAI', // Usually your GitHub org/user name.
-  projectName: '/', // Usually your repo name.
+  projectName: 'docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
